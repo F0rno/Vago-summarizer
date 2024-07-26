bee537a5c2a0a11afadd05da4542e3217c8c5c17
F0rno <pfornell@iesrafaelalberti.es>
2024-07-26 18:40:05 +0200

    good summarization, no markdown formating yet

--- video-summarizer.py
+++ video-summarizer.py
@@ -1,42 +1,17 @@
-import subprocess
 from transformers import pipeline, AutoTokenizer, AutoModelForSeq2SeqLM
 from video_processor import VideoProcessor
 from audio_transcriber import AudioTranscriber
 
-model_name = "sshleifer/distilbart-cnn-12-6"
-
-def load_model(model_name):
-    try:
-        # Try to load the cached model and tokenizer
-        model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
-        tokenizer = AutoTokenizer.from_pretrained(model_name)
-    except EnvironmentError:
-        # If loading fails, download the model
-        subprocess.run(["python3", "download_model.py"])
-        # Retry loading the model and tokenizer
-        model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
-        tokenizer = AutoTokenizer.from_pretrained(model_name)
-    return model, tokenizer
 
 def split_transcript(transcript, max_length=1024):
     return [transcript[i:i+max_length] for i in range(0, len(transcript), max_length)]
 
-def summarize_chunks(chunks, base_prompt):
+def summarize_chunks(chunks):
     summaries = []
     for chunk in chunks:
-        # Fill in the template with the current chunk
-        prompt = base_prompt.format(chunk)
-        summary = summarizer(prompt, max_length=130, min_length=30, do_sample=False)
+        summary = summarizer(chunk, max_length=130, min_length=30, do_sample=False)
         summaries.append(summary[0]['summary_text'])
     return summaries
-
-# Load the model and tokenizer
-model, tokenizer = load_model(model_name)
-
-# Create the summarizer pipeline using the cached model and tokenizer
-summarizer = pipeline("summarization", model=model, tokenizer=tokenizer, device=0)
-
-prompt_template = "Summarize the following text in Markdown format:\n\n{}"
 
 if __name__ == "__main__":
     video_path = "index.mp4"
@@ -49,12 +24,18 @@ if __name__ == "__main__":
     # Save the transcript to a file
     with open("transcript.txt", "w") as transcript_file:
         transcript_file.write(transcript)
-    
+
+    ############################################################
+    # Summarization model
+    ############################################################
+    model_name = "sshleifer/distilbart-cnn-12-6"
+    model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    summarizer = pipeline("summarization", model=model, tokenizer=tokenizer, device=0)
+
     chunks = split_transcript(transcript)
-    summaries = summarize_chunks(chunks, prompt_template)
-    
-    final_summary = " ".join(summaries)
+    summaries = summarize_chunks(chunks)
     
     # Save the summarization to a file
     with open("summary.md", "w") as summary_file:
-        summary_file.write(final_summary)
+        summary_file.write("\n".join(summaries))
